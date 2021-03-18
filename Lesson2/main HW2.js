96f8387f37317a60989ec16a01095f0ecfb1864c
Alexandr <tpkharkov@bembi.ua>
2021-03-18 15:24:55 +0200

    Lesson 2

---
+++ Lesson2/main HW2.js
@@ -0,0 +1,65 @@
+// Переменная содержит в себе строку. Вывести строку в обратном порядке.
+let str = "qwerty";
+let newStr = "";
+
+for (let i = str.length-1; i >= 0; i--) {
+    newStr += str[i];
+}
+console.log(newStr);
+
+
+// Переменная содержит в себе число. Написать скрипт который посчитает факториал этого числа.
+let factorial = 5;
+let res = factorial;
+for (let i = factorial - 1; i > 0; i--) {
+    res *= i;
+}
+console.log(res);
+
+
+//Дано число - вывести первые N делителей этого числа нацело.
+let num = 10000;
+let numDividers = 4;
+let arr = [];
+let caunt = 0;
+for (let i = 2; i <= num; i++) {
+    if (num % i == 0 && caunt < numDividers) {
+        arr.push(num / i);
+        caunt++;
+    }
+}
+console.log(arr);
+
+
+//Найти сумму цифр числа которые кратны двум
+let bigNum = 123456789;
+let bigString = bigNum + "";
+let sum = 0;
+for (i of bigString) {
+    if (i % 2 == 0) {
+        sum += +i;
+    }
+}
+console.log(sum);
+
+
+//Найти минимальное число которое больше 300 и нацело делиться на 17
+for (let i = 301; i <= (300 * 17); i++) {
+    if (i % 17 == 0) {
+        console.log(i);
+        break;
+    }
+}
+
+
+//Заданы две переменные для двух целых чисел, найти максимальное общее значение которое нацело делит два заданных числа.
+let a = 80;
+let b = 15;
+while ( a != 0 && b != 0) {
+    if (a > b) {
+        a = a % b;
+    } else {
+        b = b % a;
+    }
+}
+console.log(a + b);
